f96de87eb07786342d816b3700ea05efef44aafa
SteHun <61988201+SteHun@users.noreply.github.com>
2022-07-18 16:07:10 +0200

    Made the player and enemies move in the dummy test.py

--- game.py
+++ game.py
@@ -17,52 +17,102 @@ class game:
             raise RuntimeError("input numbers must be between 1 and 4")
         self.input = key
     def advance(self):
-        pass
+        self.player.advance()
+        for enemy in self.enemies:
+            enemy.advance()
 
 class player:
     def __init__(self):
         self.x_pos = 24
         self.y_pos = 24
+        self.max_x_pos = 44
+        self.max_y_pos = 44
+        self.min_x_pos = 4
+        self.min_y_pos = 4
+        self.x_movement = 2
+        self.y_movement = 2
+        self.is_going_down_right = True
         self.x_tile_pos = None #add later
         self.y_tile_pos = None #add later
         self.direction = directions.up
+    
     def set_direction(self, direction):
         self.direction = direction
+    
     def set_position(self, x_pos, y_pos):
         self.x_pos, self.y_pos = x_pos, y_pos
+
+    def advance(self):
+        if self.is_going_down_right:
+            if self.x_pos + self.x_movement >= self.max_x_pos or self.y_pos + self.y_movement >= self.max_y_pos:
+                self.is_going_down_right = False
+                self.advance()
+                return
+            self.x_pos += self.x_movement
+            self.y_pos += self.y_movement
+        else:
+            if self.x_pos - self.x_movement <= self.min_x_pos or self.y_pos - self.y_movement <= self.min_y_pos:
+                self.is_going_down_right = True
+                self.advance()
+                return
+            self.x_pos -= self.x_movement
+            self.y_pos -= self.y_movement
 
 class enemy:
     def __init__(self):
         self.initialize()
+
     def initialize(self):
         self.x_tile_pos = None #add later
         self.y_tile_pos = None #add later
+        self.max_x_pos, self.max_y_pos = self.x_pos + 20, self.y_pos + 20
+        self.min_x_pos, self.min_y_pos = self.x_pos - 20, self.y_pos - 20
+        self.x_movement, self.y_movement = 2, 2
+        self.is_going_down_right = True
         self.direction = directions.up
+
     def set_direction(self, direction):
         self.direction = direction
+
     def set_position(self, x_pos, y_pos):
         self.x_pos, self.y_pos = x_pos, y_pos
+    
+    def advance(self):
+        if self.is_going_down_right:
+            if self.x_pos + self.x_movement >= self.max_x_pos or self.y_pos + self.y_movement >= self.max_y_pos:
+                self.is_going_down_right = False
+                self.advance()
+                return
+            self.x_pos += self.x_movement
+            self.y_pos += self.y_movement
+        else:
+            if self.x_pos - self.x_movement <= self.min_x_pos or self.y_pos - self.y_movement <= self.min_y_pos:
+                self.is_going_down_right = True
+                self.advance()
+                return
+            self.x_pos -= self.x_movement
+            self.y_pos -= self.y_movement
 
 class blinky(enemy):
     def __init__(self):
-        self.initialize()
         self.x_pos = 40
         self.y_pos = 24
+        self.initialize()
 
 class pinky(enemy):
     def __init__(self):
-        self.initialize()
         self.x_pos = 56
         self.y_pos = 24
+        self.initialize()
 
 class inky(enemy):
     def __init__(self):
-        self.initialize()
         self.x_pos = 72
         self.y_pos = 24
+        self.initialize()
 
 class clyde(enemy):
     def __init__(self):
-        self.initialize()
         self.x_pos = 88
         self.y_pos = 24
+        self.initialize()
--- test.py
+++ test.py
@@ -45,3 +45,41 @@ class GameTests(unittest.TestCase):
             enemy.set_position(new_x_pos, new_y_pos)
             self.assertEqual(enemy.x_pos, new_x_pos)
             self.assertEqual(enemy.y_pos, new_y_pos)
+    
+    def test_player_is_moving(self):
+        game_instance = game.game()
+        for _ in range(300):
+            old_x_pos, old_y_pos = game_instance.player.x_pos, game_instance.player.y_pos
+            game_instance.advance()
+            self.assertNotEqual(old_x_pos, game_instance.player.x_pos)
+            self.assertNotEqual(old_y_pos, game_instance.player.y_pos)
+
+    def test_player_stays_in_bounds(self):
+        game_instance = game.game()
+        for _ in range(300):
+            game_instance.advance()
+            self.assertLessEqual(game_instance.player.x_pos, game_instance.player.max_x_pos)
+            self.assertLessEqual(game_instance.player.y_pos, game_instance.player.max_y_pos)
+            self.assertGreaterEqual(game_instance.player.x_pos, game_instance.player.min_x_pos)
+            self.assertGreaterEqual(game_instance.player.y_pos, game_instance.player.min_y_pos)
+    
+    def test_enemies_are_moving(self):
+        game_instance = game.game()
+        old_positions = [None for _ in range(4)]
+        for _ in range(300):
+            for i, enemy in enumerate(game_instance.enemies):
+                old_positions[i]=  game_instance.player.x_pos, game_instance.player.y_pos
+            game_instance.advance()
+            for i, enemy in enumerate(game_instance.enemies):
+                self.assertNotEqual(old_positions[i][0], game_instance.enemies[i].x_pos)
+                self.assertNotEqual(old_positions[i][1], game_instance.enemies[i].y_pos)
+    
+    def test_enemies_stay_in_bounds(self):
+        game_instance = game.game()
+        for _ in range(300):
+            game_instance.advance()
+            for enemy in game_instance.enemies:
+                self.assertLessEqual(enemy.x_pos, enemy.max_x_pos)
+                self.assertLessEqual(enemy.y_pos, enemy.max_y_pos)
+                self.assertGreaterEqual(enemy.x_pos, enemy.min_x_pos)
+                self.assertGreaterEqual(enemy.y_pos, enemy.min_y_pos)
